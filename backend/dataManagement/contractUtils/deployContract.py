0c5f8b7afa66439d9c56b3376ba922f11d51fdd7
neoff69 <76119301+neoff69@users.noreply.github.com>
2024-03-24 07:14:40 +0100

    push

--- backend/dataManagement/contractUtils/deployContract.py
+++ backend/dataManagement/contractUtils/deployContract.py
@@ -1,69 +1,69 @@
-from pytezos import pytezos
-from pytezos import ContractInterface
+# from pytezos import pytezos
+# from pytezos import ContractInterface
 
-from dataManagement.models import models, BillOfLading
+# from dataManagement.models import models, BillOfLading
 
-key = "edskS2w2qaNik7bepNQi1MinJ52ratUHUJzbnyumdLJMcfGBH9U3p8yjk3Gs1Lh84iSNNfXTbNLkEvekpYB5FTnqAFezNF4jkk"
-pytezosWallet = pytezos.using(key=key)
-
-
-
-def deployContract(bill):
-    """
-    Deploy and save the hash of the smart contract
-    Parameters
-    ----------
-    arg1 : billOfLanding object
-        bill of landing model
-
-    Returns
-    -------
-    str
-        contract's hash
-    """
-    global key, pytezosWallet
-
-    contract = ContractInterface.from_file('deployerContract.tz')
-    ci = contract.using(key=key)
-
-    value = pytezosWallet.origination(script=ci.script()).send(min_confirmations=1)
-    bill.contactHash = value
-    bill.save()
-    return value.opg_hash
+# key = "edskS2w2qaNik7bepNQi1MinJ52ratUHUJzbnyumdLJMcfGBH9U3p8yjk3Gs1Lh84iSNNfXTbNLkEvekpYB5FTnqAFezNF4jkk"
+# pytezosWallet = pytezos.using(key=key)
 
 
 
-def checkIfContractIsActive(billOfLadingID):
-    """
-    Check if the billOfLading is already existing or not
-    Parameters
-    ----------
-    arg1 : string
-        bill of lading ID
+# def deployContract(bill):
+#     """
+#     Deploy and save the hash of the smart contract
+#     Parameters
+#     ----------
+#     arg1 : billOfLanding object
+#         bill of landing model
 
-    Returns
-    -------
-    str
-        return contract hash if contract is finded
-    """
-    billOfLadingList = models.BillOfLading.objects.all()
-    for billOfLading in billOfLadingList:
-        if billOfLading.billOfLadingNumber == billOfLadingID:
-            return billOfLading.contractHash
-    return None
+#     Returns
+#     -------
+#     str
+#         contract's hash
+#     """
+#     global key, pytezosWallet
+
+#     contract = ContractInterface.from_file('deployerContract.tz')
+#     ci = contract.using(key=key)
+
+#     value = pytezosWallet.origination(script=ci.script()).send(min_confirmations=1)
+#     bill.contactHash = value
+#     bill.save()
+#     return value.opg_hash
 
 
 
-def fillContract(contractHash):
-    """
-    Fill the smart contract with the hashes
+# def checkIfContractIsActive(billOfLadingID):
+#     """
+#     Check if the billOfLading is already existing or not
+#     Parameters
+#     ----------
+#     arg1 : string
+#         bill of lading ID
+
+#     Returns
+#     -------
+#     str
+#         return contract hash if contract is finded
+#     """
+#     billOfLadingList = models.BillOfLading.objects.all()
+#     for billOfLading in billOfLadingList:
+#         if billOfLading.billOfLadingNumber == billOfLadingID:
+#             return billOfLading.contractHash
+#     return None
+
+
+
+# def fillContract(contractHash):
+#     """
+#     Fill the smart contract with the hashes
     
-    Parameters
-    ----------
-    arg1 : str
-        contract's hash
+#     Parameters
+#     ----------
+#     arg1 : str
+#         contract's hash
 
-    """
-    global key, pytezosWallet
+#     """
+#     global key, pytezosWallet
 
 
